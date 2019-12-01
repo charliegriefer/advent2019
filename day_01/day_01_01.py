e8e54fdff26ef19f214147e3c13f07e2f82b369e
Charlie Griefer <charlie@griefer.com>
2019-11-30 23:50:16 -0700

    Saved a line

--- day_01/day_01_01.py
+++ day_01/day_01_01.py
@@ -4,8 +4,7 @@ import math
 def main():
     with open("day_01.txt") as f:
         modules = [int(line) for line in f]
-    result = sum([math.floor(x/3) - 2 for x in modules])
-    print(result)
+    print(sum([math.floor(x/3) - 2 for x in modules]))
 
 
 if __name__ == "__main__":
