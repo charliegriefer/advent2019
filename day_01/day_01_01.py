a79ae3362b0ac0b2abfa6efe6cd8d77d0578b5e1
Charlie Griefer <charlie@griefer.com>
2019-12-01 00:13:56 -0700

    Changed some arg values

--- day_01/day_01_01.py
+++ day_01/day_01_01.py
@@ -1,7 +1,7 @@
 def main():
     with open("day_01.txt") as f:
         modules = [int(line) for line in f]
-    print(sum([x//3 - 2 for x in modules]))
+    print(sum([m//3 - 2 for m in modules]))
 
 
 if __name__ == "__main__":
--- day_01/day_01_02.py
+++ day_01/day_01_02.py
@@ -1,12 +1,12 @@
 def main():
     with open("day_01.txt") as f:
         modules = [int(line) for line in f]
-    print(sum([recursive_fuel(x, 0) for x in modules]))
+    print(sum([recursive_fuel(m, 0) for m in modules]))
 
 
-def recursive_fuel(n: int, s: int) -> int:
-    fuel = n//3 - 2
-    return s if fuel <= 0 else recursive_fuel(fuel, s + fuel)
+def recursive_fuel(m: int, result: int) -> int:
+    fuel = m//3 - 2
+    return result if fuel <= 0 else recursive_fuel(fuel, result + fuel)
 
 
 if __name__ == "__main__":
